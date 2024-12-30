ee3218003bb828f01d6f8eabca90094d99380d31
Pundarikaksha <pundarikaksha@PRADEEPs-MacBook-Air-2.local>
2024-12-30 23:13:14 +0530

    renamed

--- fashion_app/wsgi.py
+++ fashion_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for social_book project.
+WSGI config for fashion_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_book.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fashion_app.settings')
 
 application = get_wsgi_application()
